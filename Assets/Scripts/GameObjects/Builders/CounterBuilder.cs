a0dfa287f39e150a126fb7b284ab87987e60b3b2
ParalyzeStudio <oliviermarty.dev@gmail.com>
2015-03-30 17:00:35 +0200

    Moved Meshes/ folder inside GameObjects/

---
+++ Assets/Scripts/GameObjects/Builders/CounterBuilder.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class CounterBuilder : MonoBehaviour
+{
+    public void Build()
+    {
+        LevelManager levelManager = GameObject.FindGameObjectWithTag("LevelManager").GetComponent<LevelManager>();
+        Level currentLevel = levelManager.m_currentLevel;
+
+        int numberOfActions = 4; //TODO retrieve number of actions from currentLevel object
+    }
+}
+
