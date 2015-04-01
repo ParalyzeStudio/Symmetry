210ea5c83e4489d5bc3e0e22a90af2203e968cc2
ParalyzeStudio <oliviermarty.dev@gmail.com>
2015-04-01 19:58:22 +0200

    Added SceneAnimator and added some materials and prefabs

---
+++ Assets/Scripts/GameObjects/Meshes/Animation/SceneAnimator.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class SceneAnimator : GameObjectAnimator
+{
+    public override void OnStartFading()
+    {
+        SceneManager sceneManager = GameObject.FindGameObjectWithTag("Scenes").GetComponent<SceneManager>();
+        if (sceneManager.m_displayedContent == SceneManager.DisplayContent.LEVEL_INTRO)
+        {
+            if (m_fromOpacity == 1 && m_toOpacity == 0)
+            {
+                sceneManager.SwitchDisplayedContent(SceneManager.DisplayContent.GAME, true, 0.0f, 0.7f, 0.0f);
+            }
+        }
+    }
+}
+
