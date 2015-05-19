fc082367ecafcea5048697172420fc6713ffcdb3
ParalyzeStudio <oliviermarty.dev@gmail.com>
2015-05-20 00:22:00 +0200

    Fixed shape rendering

---
+++ Assets/Scripts/Materials/PositionColorMaterial.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class PositionColorMaterial
+{
+    public Color m_color { get; set; }
+    public Material m_material { get; set; }
+
+    public PositionColorMaterial(Color color, Material material)
+    {
+        m_color = color;
+        m_material = material;
+    }
+}
